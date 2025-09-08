a9745ad6782b89fb2fe22ba07473e858f80fe600
Omkharade318 <kharadeoms@gmail.com>
2025-09-08 21:50:46 +0530

    added functionality to add notes dynamically

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -48,17 +48,158 @@ class _HomePageState extends State<HomePage> {
       ),
 
       floatingActionButton: FloatingActionButton(
-        onPressed: () async{
-          // note to be added from here
-          bool check = await dbRef!.addNote(mTitle: "Personal Fav Note", mDesc: "This is a personal favorite note");
-          if(check) {
-            getNotes();
-          }
-      },
+        onPressed: () async {
+          // wait for bottom sheet to close
+          await showModalBottomSheet(
+            context: context,
+            builder: (context) {
+              return BottomSheetView();
+            },
+          );
+
+          // refresh notes after sheet is closed
+          getNotes();
+        },
         child: Icon(Icons.add),
       ),
     );
   }
 
+}
+
+class BottomSheetView extends StatefulWidget{
+  const BottomSheetView({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _BottomSheetViewState();
+  }
+}
+
+class _BottomSheetViewState extends State<BottomSheetView> {
+
+  // Controllers
+  TextEditingController titleController = TextEditingController();
+  TextEditingController descController = TextEditingController();
+
+  List<Map<String, dynamic>> allNotes = [];
+
+  DBHelper? dbRef;
+
+  @override
+  void initState() {
+    super.initState();
+    dbRef = DBHelper.getInstance;
+    getNotes();
+    setState(() {
+
+    });
+  }
+
+  void getNotes() async{
+    allNotes = await dbRef!.getAllNotes();
+    setState(() {
+
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(12),
+      width: double.infinity,
+      child: Column(
+        children: [
+          Text("Add Note", style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
+          SizedBox(height: 20,),
+
+          TextField(
+            controller: titleController,
+            decoration: InputDecoration(
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12)
+                ),
+                hintText: "Enter title here",
+                label: Text("Title")
+            ),
+
+          ),
+
+          SizedBox(height: 20,),
+
+          TextField(
+            controller: descController,
+            maxLines: 5,
+            decoration: InputDecoration(
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12)
+                ),
+                hintText: "Enter Description here",
+                label: Text("Description")
+            ),
+          ),
+
+          SizedBox(height: 20,),
+
+          Row(
+            children: [
+              Expanded(child: OutlinedButton(
+                  style: OutlinedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                          side: BorderSide(
+                              width: 4,
+                              color: Colors.black
+                          )
+                      )
+                  ),
+                  onPressed: () async{
+                    var title = titleController.text;
+                    var desc = descController.text;
+
+                    if(title.isNotEmpty && desc.isNotEmpty){
+                      bool check = await dbRef!.addNote(mTitle: title, mDesc: desc);
+                      if(check){
+                        getNotes();
+                        setState(() {
+
+                        });
+                      } else {
+                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Something went wrong")));
+                      }
+
+                      titleController.clear();
+                      descController.clear();
+
+                      // close the bottom sheet
+                      Navigator.pop(context);
+                    }
+                  },
+                  child: Text("Save Note"))
+              ),
+
+              SizedBox(width: 10,),
+
+              Expanded(child: OutlinedButton(
+                  style: OutlinedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                          side: BorderSide(
+                              width: 4,
+                              color: Colors.black
+                          )
+                      )
+                  ),
+                  onPressed: (){
+                    Navigator.pop(context);
+                  },
+                  child: Text("Cancel"))),
+            ],
+          )
+
+        ],
+      ),
+    );
+  }
 
 }
